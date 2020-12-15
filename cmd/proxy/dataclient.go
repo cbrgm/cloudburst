74bc7a46027205bcbcab1623637e3d337e469fc9
Christian Bargmann <chris@cbrgm.net>
2020-12-15 15:17:21 +0100

    feat: wip: add proxy dataclient implementation

--- cmd/proxy/dataclient.go
+++ cmd/proxy/dataclient.go
@@ -4,23 +4,27 @@ import (
 	"context"
 	"fmt"
 	apiclient "github.com/cbrgm/cloudburst/api/client/go"
+	"github.com/cbrgm/cloudburst/cloudburst"
+	"github.com/cbrgm/cloudburst/cloudburst/convert"
+	"github.com/go-kit/kit/log/level"
 	"github.com/zalando/skipper/eskip"
 	"log"
+	"net/http"
 	"net/url"
 )
 
 type Client struct {
 	client  *apiclient.APIClient
 	current map[string]*eskip.Route
+	logger  log.Logger
 }
 
 // NewCloudburst creates a data client that parses a string of eskip Client and
 // serves it for the routing package.
-func NewCloudburst(prometheusURL string) (*Client, error) {
-
+func NewCloudburst(logger log.Logger, prometheusURL string) (*Client, error) {
 	apiURL, err := url.Parse(prometheusURL)
 	if err != nil {
-		return nil, fmt.Errorf("failed to parse API URL: %w", err)
+		return nil, fmt.Errorf("failed to parse API URL: %v", err)
 	}
 
 	clientCfg := apiclient.NewConfiguration()
@@ -31,31 +35,102 @@ func NewCloudburst(prometheusURL string) (*Client, error) {
 
 	return &Client{
 		client:  client,
+		logger:  logger,
 		current: make(map[string]*eskip.Route),
 	}, nil
 }
 
 func (c *Client) loadAndConvert() ([]*eskip.Route, error) {
-	targets, resp, err := c.client.TargetsApi.ListScrapeTargets(context.TODO()).Execute()
-	if err != nil {
-		return []*eskip.Route{}, err
+	res, resp, err := c.client.TargetsApi.ListScrapeTargets(context.TODO()).Execute()
+	if err != nil || resp.StatusCode != http.StatusOK {
+		return nil, err
 	}
 
-	for _, target := range targets {
-		instances, resp, err := c.client.InstancesApi.GetInstances(context.TODO(), target.Name).Execute()
-		if err != nil {
-			return []*eskip.Route{}, err
-		}
+	scrapeTargets := convert.APIClientToScrapeTargets(res)
+
+	r, err := c.routesForScrapeTargets(scrapeTargets)
+	if err != nil {
+		return nil, err
 	}
-	return nil, nil
+
+	return r, nil
+}
+
+func (c *Client) routesForScrapeTargets(scrapeTargets []*cloudburst.ScrapeTarget) ([]*eskip.Route, error) {
+	var res []*eskip.Route
+	for _, target := range scrapeTargets {
+		route, err := c.routeForScrapeTarget(target)
+		if err != nil {
+			return res, err
+		}
+		res = append(res, route)
+	}
+	return res, nil
+}
+
+func (c *Client) routeForScrapeTarget(scrapeTarget cloudburst.ScrapeTarget) (*eskip.Route, error) {
+	res, resp, err := c.client.InstancesApi.GetInstances(context.TODO(), scrapeTarget.Name).Execute()
+	if err != nil || resp.StatusCode != http.StatusOK {
+		return nil, err
+	}
+
+	instances := convert.APIClientToInstances(res)
+
+	lbEndpoints := []string{}
+	for _, instance := range instances {
+		lbEndpoints = append(lbEndpoints, instance.Endpoint)
+	}
+
+	return &eskip.Route{
+		Id:          scrapeTarget.Name,
+		Predicates:  nil,
+		Filters:     nil,
+		BackendType: 0,
+		Backend:     "",
+		LBAlgorithm: "",
+		LBEndpoints: lbEndpoints,
+	}
+}
+
+func mapRoutes(r []*eskip.Route) map[string]*eskip.Route {
+	m := make(map[string]*eskip.Route)
+	for _, ri := range r {
+		m[ri.Id] = ri
+	}
+
+	return m
 }
 
 func (c *Client) LoadAll() ([]*eskip.Route, error) {
-	log.Println("loading Client")
-	return nil, nil
+	level.Debug(c.logger).Log("msg", "loading routes")
+
+	r, err := c.loadAndConvert()
+	if err != nil {
+		return nil, fmt.Errorf("failed to load routes from cloudburst-api: %v", err)
+	}
+
+	c.current = mapRoutes(r)
+	level.Debug(c.logger).Log("msg", "all routes loaded and mapped")
+
+	return r, nil
 }
 
 func (c *Client) LoadUpdate() ([]*eskip.Route, []string, error) {
-	log.Println("updating Client")
+	level.Debug(c.logger).Log("msg", "updating routes")
+
+	r, err := c.loadAndConvert()
+	if err != nil {
+		level.Error(c.logger).Log("msg", "polling for updates failed: %v", err)
+		return nil, nil, fmt.Errorf("polling for updates from cloudburst-api failed: %s", err)
+	}
+
+	next := mapRoutes(r)
+	level.Debug(c.logger).Log("msg", "next version of routes loaded and mapped")
+
+	var (
+		updatedRoutes []*eskip.Route
+		deletedIDs    []string
+	)
+
 	return nil, nil, nil
 }
--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"errors"
 	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+	"github.com/oklog/run"
+	"github.com/pkg/errors"
 	"github.com/urfave/cli"
 	"os"
 
@@ -12,7 +14,9 @@ import (
 
 const (
 	flagApiAddr = "api.url"
+	flagAddr    = "addr"
 )
+
 func main() {
 	logger := log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
 	logger = log.WithPrefix(logger, "ts", log.DefaultTimestampUTC)
@@ -24,19 +28,28 @@ func main() {
 	app.Action = proxyAction(logger)
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
+			Name:  flagAddr,
+			Usage: "The address for the public http server",
+			Value: ":6661",
+		},
+		cli.StringFlag{
 			Name:  flagApiAddr,
 			Usage: "FQDN for the cloudburst-api to connect to, in format http://localhost:6660",
 		},
 	}
 
 	if err := app.Run(os.Args); err != nil {
-		logger.Log("msg", "failed running agent", "err", err)
+		logger.Log("msg", "failed running proxy", "err", err)
 		os.Exit(1)
 	}
 }
 
 func proxyAction(logger log.Logger) cli.ActionFunc {
 	return func(c *cli.Context) error {
+
+		if c.String(flagApiAddr) == "" {
+			return errors.New("no proxy addr provided, please set an address to bind on using --addr flag")
+		}
 
 		if c.String(flagApiAddr) == "" {
 			return errors.New("no api.url provided, please set the remote cloudburst-api url using --api.url flag")
@@ -47,11 +60,26 @@ func proxyAction(logger log.Logger) cli.ActionFunc {
 			logger.Log("msg", "failed to create client for proxy", "err", err)
 		}
 
-		skipper.Run(skipper.Options{
-			Address:           ":9090",
-			CustomDataClients: []routing.DataClient{client},
-		})
+		var gr run.Group
+		{
+			gr.Add(func() error {
+				level.Info(logger).Log(
+					"msg", "running HTTP proxy server",
+					"addr", c.String(flagAddr),
+				)
+				err := skipper.Run(skipper.Options{
+					Address:           c.String(flagAddr),
+					CustomDataClients: []routing.DataClient{client},
+				})
+				return err
+			}, func(err error) {
 
+			})
+		}
+
+		if err := gr.Run(); err != nil {
+			return errors.Errorf("error running: %w", err)
+		}
 		return nil
 	}
 }
