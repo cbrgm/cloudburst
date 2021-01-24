bc29fac93561259d52a2971e4c6d463a11a9747c
Christian Bargmann <chris@cbrgm.net>
2021-01-24 02:15:33 +0100

    fix: edit experiments

--- experiments/benchmark.sh
+++ experiments/benchmark.sh
@@ -9,13 +9,14 @@ benchmark_results () {
 }
 
 get_benchmark_data () {
-  styx gnuplot --prometheus http://localhost:9090 --duration 5m 'rate(cloudburst_proxy_custom_total{key="incoming.HTTP/1.1"}[30s])' > http_requests_total.gnuplot
-  styx gnuplot --prometheus http://localhost:9090 --duration 5m 'rate(cloudburst_proxy_custom_total{key="incoming.HTTP/1.1"}[30s]) / 15' > http_requests_slo.gnuplot
-  styx gnuplot --prometheus http://localhost:9090 --duration 5m 'cloudburst_api_instances_total{status="running"} + 1' > instances_total.gnuplot
-  styx gnuplot --prometheus http://localhost:9090 --duration 5m 'sum(rate(example_sorting_requests_total[30s]))' > http_requests_single_service.gnuplot
+  styx matplotlib --prometheus http://localhost:9090 --duration 5m 'rate(cloudburst_proxy_custom_total{key="incoming.HTTP/1.1"}[30s])' > $1/http_requests_total.py
+  styx matplotlib --prometheus http://localhost:9090 --duration 5m 'rate(cloudburst_proxy_custom_total{key="incoming.HTTP/1.1"}[30s]) / 15' > $1/http_requests_slo.py
+  styx matplotlib --prometheus http://localhost:9090 --duration 5m 'cloudburst_api_instances_total{status="running"} + 1' > $1/instances_total.py
+  styx matplotlib --prometheus http://localhost:9090 --duration 5m 'sum(rate(example_sorting_requests_total[30s]))' > $1/http_requests_single_service.py
 }
 
 for filename in workloads/*.yaml; do
+  echo "Running benchmark file $filename"
   docker run --rm -it --network="host" -v $(pwd):/workload cbrgm/artillery:latest /bin/sh -c "artillery run ${filename}"
   benchmark_results $filename
 done
