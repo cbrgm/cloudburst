03d339d5119e9a05e86a1737f630a6b436c5d004
Christian Bargmann <chris@cbrgm.net>
2021-05-08 20:20:21 +0200

    feat: edit benchmark tests to use new metrics

--- experiments/benchmark.sh
+++ experiments/benchmark.sh
@@ -13,9 +13,9 @@ get_benchmark_data () {
   styx matplotlib --prometheus http://localhost:9090 --duration 10m 'rate(cloudburst_proxy_custom_total{key="incoming.HTTP/1.1"}[30s])' > $1/http_requests_total.py
   styx matplotlib --prometheus http://localhost:9090 --duration 10m '(rate(cloudburst_proxy_custom_total{key="incoming.HTTP/1.1"}[30s]) / 5)' > $1/http_requests_slo.py
   styx matplotlib --prometheus http://localhost:9090 --duration 10m 'cloudburst_calculator_instances_total{status="running"} + 1' > $1/instances_total_running.py
-  styx matplotlib --prometheus http://localhost:9090 --duration 10m 'cloudburst_calculator_instances_total{provider="cloud-provider-A",status="running"}' > $1/instances_total_provider_a.py
-  styx matplotlib --prometheus http://localhost:9090 --duration 10m 'cloudburst_calculator_instances_total{provider="cloud-provider-B",status="running"}' > $1/instances_total_provider_b.py
-  styx matplotlib --prometheus http://localhost:9090 --duration 10m 'cloudburst_calculator_instances_total{provider="cloud-provider-C",status="running"}' > $1/instances_total_provider_c.py
+  styx matplotlib --prometheus http://localhost:9090 --duration 10m 'cloudburst_calculator_instances_total{provider="cloud-provider-alpha",status="running"}' > $1/instances_total_provider_alpha.py
+  styx matplotlib --prometheus http://localhost:9090 --duration 10m 'cloudburst_calculator_instances_total{provider="cloud-provider-beta",status="running"}' > $1/instances_total_provider_beta.py
+  styx matplotlib --prometheus http://localhost:9090 --duration 10m 'cloudburst_calculator_instances_total{provider="cloud-provider-gamma",status="running"}' > $1/instances_total_provider_gamma.py
   styx matplotlib --prometheus http://localhost:9090 --duration 10m 'sum(rate(example_sorting_requests_total[30s])) * 10' > $1/http_requests_single_service.py
   echo "done!"
   sleep 120
